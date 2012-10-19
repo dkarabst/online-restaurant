51da8469d9c5d61577f003ecf825a2293e519109
ppnof2012 <ppnof2012@63853741-424a-8dcd-377a-bba2f548af83>
2012-10-19 16:23:12 +0000

    User.java entity was changed

--- web/src/main/java/com/softserve/academy/food/entity/User.java
+++ web/src/main/java/com/softserve/academy/food/entity/User.java
@@ -10,79 +10,77 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "USERS")
-public class User extends AbsEntity
-{
+public class User extends AbsEntity {
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name = "user_id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer				id;
+	@Column(name = "USER_ID")
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Integer id;
 
 	@Column(name = "USER_NAME", length = 50)
-	private String				name;
+	private String name;
 
 	@Column(name = "USER_EMAIL", length = 50)
-	private String				email;
+	private String email;
 
-	public User()
-	{
-	}
+	@Column(name = "USER_PHONE", length = 50)
+	private String phone;
 
-	public User(String name, String email)
-	{
+	public User() {}
+	public User(Integer id, String name, String email, String phone) {
+		setId(id);
 		setName(name);
 		setEmail(email);
+		setPhone(phone);
 	}
 
-	public Integer getId()
-	{
+
+
+	public Integer getId() {
 		return id;
 	}
-
-	public void setId(Integer id)
-	{
+	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
-
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getEmail()
-	{
+	public String getEmail() {
 		return email;
 	}
-
-	public void setEmail(String email)
-	{
+	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
 		return result;
 	}
-
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -108,8 +106,13 @@ public class User extends AbsEntity
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (phone == null)
+		{
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
 		return true;
 	}
-
+	
 } // User
-
