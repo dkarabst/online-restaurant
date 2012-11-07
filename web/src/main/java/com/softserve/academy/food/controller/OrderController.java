3ce065911f578fe0a0b155da6b3c25c2b059ac64
www.dimuha@gmail.com <www.dimuha@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-07 18:30:08 +0000

    added  deleteDish method  to remove dish from the list.

--- web/src/main/java/com/softserve/academy/food/controller/OrderController.java
+++ web/src/main/java/com/softserve/academy/food/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.softserve.academy.food.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
@@ -22,10 +23,10 @@ public class OrderController
 {
 
 	@Autowired
-	private OrderService	orderService;
-	
-	//temporal
-	private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	private OrderService				orderService;
+
+	// temporal
+	private HashMap<Integer, Integer>	map	= new HashMap<Integer, Integer>();
 
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	protected String categoryList(Model model)
@@ -47,29 +48,55 @@ public class OrderController
 	}
 
 	@RequestMapping(value = "/addDish", method = RequestMethod.POST)
-	public String addDish(@RequestParam("dish_id") int dish_id,@RequestParam("quantity") int quantity, Model model,
+	public String addDish(@RequestParam("dish_id") int dish_id,
+			@RequestParam("quantity") int quantity, Model model,
 			HttpSession session)
 	{
-//		@SuppressWarnings("unchecked")
-		// HashMap<Integer,Integer> map  must be created my sign in..  
-//		HashMap<Integer,Integer> map = (HashMap<Integer,Integer>) session.getAttribute("dishList");
-		if(map.containsKey(dish_id))
+		// @SuppressWarnings("unchecked")
+		// HashMap<Integer,Integer> map must be created my sign in..
+		// HashMap<Integer,Integer> map = (HashMap<Integer,Integer>)
+		// session.getAttribute("dishList");
+		if (map.containsKey(dish_id))
 		{
-			map.put(dish_id, (map.get(dish_id)+quantity));
-		}
-		else
+			map.put(dish_id, (map.get(dish_id) + quantity));
+		} else
 		{
 			map.put(dish_id, quantity);
 		}
 		session.setAttribute("dishList", map);
+		System.out.println(Arrays.asList(session.getAttribute("dishList")));
+		return "redirect:../order";
+	}
+
+	@RequestMapping(value = "/deleteDish", method = RequestMethod.POST)
+	public String deleteDish(@RequestParam("dish_id") int dish_id,
+			@RequestParam("quantity") int quantity, Model model,
+			HttpSession session)
+	{
+		// @SuppressWarnings("unchecked")
+		// HashMap<Integer,Integer> map must be created my sign in..
+		// HashMap<Integer,Integer> map = (HashMap<Integer,Integer>)
+		// session.getAttribute("dishList");
+		if (map.containsKey(dish_id))
+		{
+			if (map.get(dish_id) > quantity)
+			{
+				map.put(dish_id, (map.get(dish_id) - quantity));
+			} else
+			{
+				map.remove(dish_id);
+			}
+			session.setAttribute("dishList", map);
+		}
+		System.out.println(Arrays.asList(session.getAttribute("dishList")));
 		return "redirect:../order";
 	}
 
 	@RequestMapping(value = "/makeOrder", method = RequestMethod.GET)
-	public String makeOrder( Model model,HttpSession session)
+	public String makeOrder(Model model, HttpSession session)
 	{
 		// temporal
-//		Integer user_id = session.getAttribute("user_id");
+		// Integer user_id = session.getAttribute("user_id");
 		// user id =1 , because we dont have security now
 		orderService.add(1, session.getAttribute("dishList"));
 		return "redirect:../order";
