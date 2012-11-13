ccc5079773ced30408b1113ac26007bea7a3ca48
Trancelic <Trancelic@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-13 12:02:42 +0000

    Change entity Attachment

---
+++ webserver/main/java/com/softserve/academy/food/entity/DishAttachment.java
@@ -0,0 +1,56 @@
+package com.softserve.academy.food.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ATTACHMENTS")
+@PrimaryKeyJoinColumn(name="ATTACHMENTS_ID", referencedColumnName="ATTACHMENTS_ID")
+public class DishAttachment extends Attachment {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+    private Dish dish;
+
+    public DishAttachment()
+    {
+    }
+
+	public Dish getDish() {
+		return dish;
+	}
+
+	public void setDish(Dish dish) {
+		this.dish = dish;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((dish == null) ? 0 : dish.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof DishAttachment))
+			return false;
+		DishAttachment other = (DishAttachment) obj;
+		if (dish == null) {
+			if (other.dish != null)
+				return false;
+		} else if (!dish.equals(other.dish))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DishAttachment [dish=" + dish + "]";
+	}
+ 
+
+}
