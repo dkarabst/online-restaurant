839547889024b7ccf04e5b747701f458457e9123
chubarov.sv@gmail.com <chubarov.sv@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-16 17:41:54 +0000

    add attachments service

---
+++ trunk/web/src/main/java/com/softserve/academy/food/service/impl/UserAttachmentService.java
@@ -0,0 +1,38 @@
+package com.softserve.academy.food.service;
+
+import com.softserve.academy.food.dao.IUserAttachmentDao;
+import com.softserve.academy.food.entity.UserAttachment;
+import com.softserve.academy.food.model.UserAttachmentModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserAttachmentService implements IUserAttachmentService {
+
+    @Autowired
+    IUserAttachmentDao attachmentDao;
+
+    @Override
+    public UserAttachmentModel setModel(String name, String path) {
+        return new UserAttachmentModel(name, path);
+    }
+
+    @Override
+    public void add(UserAttachmentModel attachmentModel) {
+        attachmentDao.add(attachmentModel.toEntity());
+    }
+
+    @Override
+    public List<UserAttachmentModel> getAll() {
+        List<UserAttachmentModel> attachmentModelList = new ArrayList<UserAttachmentModel>();
+        for(UserAttachment attachment : attachmentDao.getAll()) {
+                attachmentModelList.add(attachment.toModel());
+        }
+        return attachmentModelList;
+    }
+}
