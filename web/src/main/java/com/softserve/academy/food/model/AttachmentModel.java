0a8fe86d4212968acf0baeadf99f667a6455a6df
chubarov.sv@gmail.com <chubarov.sv@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-16 17:38:47 +0000

    Update attachments (Model, Entity, Service, Controller, userinfo.jsp)

--- web/src/main/java/com/softserve/academy/food/model/AttachmentModel.java
+++ web/src/main/java/com/softserve/academy/food/model/AttachmentModel.java
@@ -8,6 +8,19 @@ public class AttachmentModel {
     private String name;
     private String path;
 
+    public AttachmentModel() {}
+
+    public AttachmentModel(Attachment attachment) {
+        id = attachment.getId();
+        name = attachment.getName();
+        path = attachment.getPath();
+    }
+
+    public AttachmentModel(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
     public Long getId() {
         return id;
     }
@@ -33,15 +46,12 @@ public class AttachmentModel {
     }
 
     public Attachment toEntity() {
-        Attachment attachment = new Attachment();
-        attachment.setId(id);
-        attachment.setName(name);
-        attachment.setPath(path);
-        return attachment;
+        return new Attachment(this);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -51,7 +61,8 @@ public class AttachmentModel {
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj)
+    {
         if (this == obj)
             return true;
         if (obj == null)
@@ -59,17 +70,20 @@ public class AttachmentModel {
         if (getClass() != obj.getClass())
             return false;
         AttachmentModel other = (AttachmentModel) obj;
-        if (id == null) {
+        if (id == null)
+        {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (name == null) {
+        if (name == null)
+        {
             if (other.name != null)
                 return false;
         } else if (!name.equals(other.name))
             return false;
-        if (path == null) {
+        if (path == null)
+        {
             if (other.path != null)
                 return false;
         } else if (!path.equals(other.path))
