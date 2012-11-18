07f473b40abedf0baa95b48834465b1e55ebb565
Trancelic <Trancelic@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-18 20:06:42 +0000

    fix autowired fields

--- web/src/main/java/com/softserve/academy/food/controller/HistoryController.java
+++ web/src/main/java/com/softserve/academy/food/controller/HistoryController.java
@@ -1,7 +1,7 @@
 package com.softserve.academy.food.controller;
 
 import com.softserve.academy.food.model.OrderModel;
-import com.softserve.academy.food.service.impl.OrderHistoryService;
+import com.softserve.academy.food.service.IOrderHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,7 @@ import java.util.Map;
 public class HistoryController {
 
     @Autowired
-    private OrderHistoryService orderHistoryService;
+    private IOrderHistoryService orderHistoryService;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listContacts(Map<String, Object> map) {
