500da2eacc2d6c174aec8b7ff07e10924c5c6f87
www.dimuha@gmail.com <www.dimuha@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-19 20:25:23 +0000

    TODO  done

--- web/src/main/java/com/softserve/academy/food/controller/MenuController.java
+++ web/src/main/java/com/softserve/academy/food/controller/MenuController.java
@@ -1,7 +1,10 @@
 package com.softserve.academy.food.controller;
 
-import com.softserve.academy.food.model.DishModel;
-import com.softserve.academy.food.service.IMenuService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,27 +12,43 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
+import com.softserve.academy.food.model.CategoryModel;
+import com.softserve.academy.food.model.DishModel;
+import com.softserve.academy.food.service.ICategoryService;
+import com.softserve.academy.food.service.IMenuService;
 
 @Controller
-public class MenuController {
+public class MenuController
+{
 
-    @Autowired
-    private IMenuService dishService;
+	@Autowired
+	private IMenuService		dishService;
 
-    @RequestMapping(value = "/dishes/all", method = RequestMethod.GET)
-    protected String getMenu(Model model) {
-        //TODO return model with map category:dishList
-        return "helloPage";
-    }
+	@Autowired
+	private ICategoryService	categoryService;
 
-    @RequestMapping(value = "/dishes/{id}", method = RequestMethod.GET)
-    protected String getDish(@PathVariable int id, Model model) {
-        ArrayList<String> names = new ArrayList<String>();
-        DishModel mod = dishService.getDish(id);
-        names.add(mod.toString());
-        model.addAttribute("helloMessage", names);
-        return "helloPage";
-    }
+	// returning map of CategoryModel - List<DishModel>
+	// as TODO was
+	@RequestMapping(value = "/dishes/all", method = RequestMethod.GET)
+	protected String getMenu(Model model)
+	{
+		Map<CategoryModel, List<DishModel>> map = new HashMap<CategoryModel, List<DishModel>>();
+		for (CategoryModel c : categoryService.getAll())
+		{
+			map.put(c, dishService.getAllDishesForCategory(c));
+		}
+		model.addAttribute("helloMessage", map);
+		return "helloPage";
+	}
+
+	@RequestMapping(value = "/dishes/{id}", method = RequestMethod.GET)
+	protected String getDish(@PathVariable int id, Model model)
+	{
+		ArrayList<String> names = new ArrayList<String>();
+		DishModel mod = dishService.getDish(id);
+		names.add(mod.toString());
+		model.addAttribute("helloMessage", names);
+		return "helloPage";
+	}
 
 }
