aab7e33de2650207b69dbfc88654227c0a7dc61c
reutaov@gmail.com <reutaov@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-20 20:38:56 +0000

    All scripts are udated to the new web project

---
+++ trunk/DB/DB/cat.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM restaurant.categories
+LIMIT 0, 1000
+
+-- Date: 2012-11-19 13:53
+*/
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (1,'Салаты','images/dishes/cat01/Category page_salads.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (2,'Суши','/images/dishes/cat02/Category page_sushi.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (3,'Роллы','/images/dishes/cat03/Category page_rolls.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (4,'Рыбные блюда','/images/dishes/cat04/Category page_fishdish.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (5,'Горячие закуски','/images/dishes/cat05/Category page_Hot appetizers.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (6,'Первые блюда','/images/dishes/cat06/Category page_First course.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (7,'Напитки безалкогольные','/images/dishes/cat07/Category page_Non alcohol drinks.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (8,'Коктейли','/images/dishes/cat08/Category page_Coctails.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (9,'Холодные закуски','/images/dishes/cat09/Category page_Cold.jpg');
+INSERT INTO `categories` (`CAT_ID`,`CAT_NAME`,`CAT_PHOTO`) VALUES (10,'Десерты','/images/dishes/cat10/Category page_Deserts.jpg');
