eaf5eac4d7dd1c8eac2239054638ed71d8acec19
reutaov@gmail.com <reutaov@gmail.com@63853741-424a-8dcd-377a-bba2f548af83>
2012-11-23 00:19:32 +0000

    good LoginActivity + userModel in globals

--- android/src/com/softserve/academy/food/android/Role.java
+++ android/src/com/softserve/academy/food/android/Role.java
@@ -1,10 +1,31 @@
 package com.softserve.academy.food.android;
 
 import android.app.Application;
+import com.softserve.academy.food.android.model.UserModel;
 
 public class Role extends Application
 {
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public UserModel getUserModel() {
+		return userModel;
+	}
+
+	public void setUserModel(UserModel userModel) {
+		this.userModel = userModel;
+	}
+
 	private int currentRole = 2;
+	
+	private String login = "guest";
+	
+	private UserModel userModel;
 
 	public void setAdmin()
 	{
